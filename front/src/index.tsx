2c21450cb491d7a6f74f8ac1fc4af89ccc5d9622
EvgeniaM6 <evgminukova6@gmail.com>
2023-12-28 18:05:47 +0200

    init: start client side app

---
+++ front/src/index.tsx
@@ -0,0 +1,12 @@
+import './sass/style.scss';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './script/App';
+// import { Provider } from 'react-redux';
+// import store from './script/store';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
---
+++ front/src/script/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => {
+  console.log('Hello world!');
+
+  return <div>Hello world!</div>;
+};
