f8f9eb96039d64083c024253ccdebc4e331df69d
EvgeniaM6 <evgminukova6@gmail.com>
2023-12-28 15:37:06 +0200

    feat: create bicycle data model

---
+++ server/src/models/bicycle.model.ts
@@ -0,0 +1,18 @@
+enum BICYCLE_STATUS {
+  AVAILABLE = 'available',
+  BUSY = 'busy',
+  UNAVAILABLE = 'unavailable',
+}
+
+type BicycleData = {
+  id: string;
+  name: string;
+  type: string;
+  color: string;
+  wheelSize: string;
+  price: number;
+  description: string;
+  status: BICYCLE_STATUS;
+};
+
+export { BICYCLE_STATUS, BicycleData };
