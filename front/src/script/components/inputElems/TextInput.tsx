bf3a1d37206f1ba6756ed779fe59a7da6f4e9244
EvgeniaM6 <evgminukova6@gmail.com>
2023-12-30 21:14:38 +0200

    feat: add validation for unique id

--- front/src/script/components/inputElems/TextInput.tsx
+++ front/src/script/components/inputElems/TextInput.tsx
@@ -1,6 +1,9 @@
-import { TextInputProps } from '../../models';
+import { BicycleDBData, TextInputProps } from '../../models';
+import { useGetAllBicyclesQuery } from '../../redux/bicycleApi';
 
 export const TextInput = ({ id, placeholder, register, errors }: TextInputProps) => {
+  const { data } = useGetAllBicyclesQuery('');
+
   const error = errors[id];
   const minSymbols = 5;
 
@@ -17,6 +20,13 @@ export const TextInput = ({ id, placeholder, register, errors }: TextInputProps)
           minLength: {
             value: minSymbols,
             message: `Minimum ${minSymbols} symbols`,
+          },
+          validate: (value: string) => {
+            if (id !== 'id' || !data) return true;
+            const foundIndexSameId = (data as BicycleDBData[]).findIndex(
+              (bicycle: BicycleDBData) => bicycle.id === value
+            );
+            return foundIndexSameId === -1 || 'This id already exists. Enter another';
           },
         })}
       />
